9cdfaa1f34e4623d0beabddebdd29a45fb132efc
tamakiii <tamakiii@users.noreply.github.com>
2020-05-03 02:10:32 +0900

    split into api with docker network

---
+++ api/docker.mk
@@ -0,0 +1,37 @@
+.PHONY: help install install-dev dependencies build clean
+
+# PROJECT_NAME := xxx-blog
+ENVIRONMENT := production-pseudo
+
+export DOCKER_BUILDKIT := 1
+export COMPOSE_DOCKER_CLI_BUILD := 1
+
+help:
+	@cat $(firstword $(MAKEFILE_LIST))
+
+install: \
+	dependencies \
+	.env
+
+install-dev: \
+	development \
+	install
+
+dependencies:
+	type docker > /dev/null
+	type docker-compose > /dev/null
+
+build:
+	docker-compose build
+
+.env:
+	# echo "PROJECT_NAME=$(PROJECT_NAME)" > $@
+	echo "ENVIRONMENT=$(ENVIRONMENT)" > $@
+	echo "ALB_PORT=$(ALB_PORT)" >> $@
+
+clean:
+	rm -rf .env
+
+.PHONY: development
+development:
+	$(eval ENVIRONMENT := development)
