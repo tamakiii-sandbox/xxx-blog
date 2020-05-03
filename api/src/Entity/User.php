cdf7c70dafaa9092c09e4ca1fc20af1c6798b8fc
tamakiii <tamakiii@users.noreply.github.com>
2020-05-03 15:12:29 +0900

    bin/console make:entity

---
+++ api/src/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $display_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDisplayName(): ?string
+    {
+        return $this->display_name;
+    }
+
+    public function setDisplayName(string $display_name): self
+    {
+        $this->display_name = $display_name;
+
+        return $this;
+    }
+}
