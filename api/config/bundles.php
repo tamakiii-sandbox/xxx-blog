8f66ea605582b8fec028780c214d989009384113
tamakiii <tamakiii@users.noreply.github.com>
2020-05-03 19:58:22 +0900

    composer require annotations

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyBundles\JsonRequestBundle\SymfonyBundlesJsonRequestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
