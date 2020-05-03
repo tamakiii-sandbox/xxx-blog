07fdff3c2f9ca8bb8c26e92f8bba8f78f074f096
tamakiii <tamakiii@users.noreply.github.com>
2020-05-03 20:01:01 +0900

    tmp impl

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,9 +1,43 @@
 import React, { useState, useEffect } from "react";
 
+interface User {
+  id: number;
+  display_name: string;
+}
+
 export default function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    (async () => {
+      const url = new URL('/users', 'https://localhost/');
+      console.log(url.toString());
+      const response = await fetch(url.toString(), {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+          "X-REQUESTED-WITH": "XMLHttpRequest",
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        }
+      })
+
+      if (!response.ok) {
+        const body = (await response.json());
+        // return { ok: false, body }
+      }
+
+      const body = (await response.json());
+      console.log(body);
+      // return { ok: true, body };
+    })()
+  }, [])
+
   return (
     <>
-      <h1>Hello, World</h1>
+      <h1>Users</h1>
+      {}
+      <ul></ul>
+      <hr />
     </>
   )
 }
