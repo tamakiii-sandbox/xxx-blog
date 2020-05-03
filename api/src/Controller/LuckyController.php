445bdb31d0688f2fd8bbbcecc87b7649a967c0d1
tamakiii <tamakiii@users.noreply.github.com>
2020-05-03 16:01:10 +0900

    load from database

--- api/src/Controller/LuckyController.php
+++ api/src/Controller/LuckyController.php
@@ -1,17 +1,29 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 class LuckyController
 {
-    public function number()
+    public function number(EntityManagerInterface $em)
     {
         $number = random_int(0, 100);
 
+        $repository = $em->getRepository(User::class);
+        $users = $repository->findAll();
+
         return new JsonResponse([
           'luckey_number' => $number,
+          'users' => array_map(
+              function(User $user) {
+                  return [
+                      'id' => $user->getId(),
+                      'display_name' => $user->getDisplayName(),
+                  ];
+              }, $users),
         ]);
     }
 }
